fd6fa29c5a9b7b27d016cf8737e0e34c306c8652
Roman <romanshliakhov@gmail.com>
2022-09-01 12:56:08 +0300

    fix gray block

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,14 +1,14 @@
-import './components/sliders.js';
+// import './components/sliders.js';
 import './components/select.js';
-import './components/burger.js';
+// import './components/burger.js';
 import './components/spollers.js';
 import './components/dropdown.js';
-import './components/tabs.js';
+// import './components/tabs.js';
 import './components/modals.js';
 import './components/sticky.js';
 import './components/accordion.js';
 import './components/replaceEl.js';
-import './components/poper.js';
+// import './components/poper.js';
 
 
 
